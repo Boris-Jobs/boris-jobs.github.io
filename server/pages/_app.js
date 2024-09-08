4d4cbbbfb3cf9976123b541f4ef1412a945b5347
boris sigma <1322553126@qq.com>
2024-09-08 15:05:56 +0800

    Updates

---
+++ server/pages/_app.js
@@ -0,0 +1,1 @@
+(()=>{var r={};r.id=888,r.ids=[888],r.modules={414:(r,e,t)=>{"use strict";t.r(e),t.d(e,{default:()=>u});var s=t(997);t(600),t(517);let u=function({Component:r,pageProps:e}){return s.jsx(r,{...e})}},600:()=>{},517:()=>{},997:r=>{"use strict";r.exports=require("react/jsx-runtime")}};var e=require("../webpack-runtime.js");e.C(r);var t=e(e.s=414);module.exports=t})();
