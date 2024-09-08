889cf95d3038b0f475ac12269b4a985470b68170
boris sigma <1322553126@qq.com>
2024-09-08 17:17:18 +0800

    updated

--- components/SEO/Meta.js
+++ components/SEO/Meta.js
@@ -7,12 +7,12 @@ export default function Meta() {
            /* Primary Meta Tags */
             <title>Boris Jobs Portfolio - Future AI Engineer</title>
             <meta charSet="utf-8" />
-            <meta name="title" content="Vivek Patel Portfolio - Computer Engineering Student" />
+            <meta name="title" content="boris jobs Portfolio - Computer Engineering Student" />
             <meta name="description"
                 content="Boris Jobs's Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS." />
             <meta name="author" content="Boris Jobs" />
             <meta name="keywords"
-                content="vivek9patel, vivek9patel's portfolio, vivek9patel linux, ubuntu portfolio, vivek patel protfolio,vivek patel computer, vivek patel, vivek ubuntu, vivek patel ubuntu portfolio" />
+                content="boris jobs, borisjobs's portfolio, borisjobs linux, ubuntu portfolio, boris jobs protfolio,boris jobs computer, boris jobs, boris ubuntu, boris jobs ubuntu portfolio" />
             <meta name="robots" content="index, follow" />
             <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
             <meta name="language" content="English" />
@@ -20,32 +20,38 @@ export default function Meta() {
             <meta name="theme-color" content="#E95420" />
 
             /* Search Engine */
-            <meta name="image" content="images/logos/fevicon.png" />
+            <meta name="image" content="images/logos/alexander-modified.png" /> 
+            {/* fevicon.png */}
             /* Schema.org for Google */
-            <meta itemProp="name" content="Vivek Patel Portfolio - Computer Engineering Student" />
+            <meta itemProp="name" content="boris jobs Portfolio - Computer Engineering Student" />
             <meta itemProp="description"
-                content="Vivek Patel's (vivek9patel) Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS." />
-            <meta itemProp="image" content="images/logos/fevicon.png" />
+                content="boris jobs's (borisjobs) Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS." />
+            <meta itemProp="image" content="images/logos/alexander-modified.png" />
+            {/* fevicon.png */}
             /* Twitter */
             <meta name="twitter:card" content="summary" />
-            <meta name="twitter:title" content="Vivek Patel Portfolio - Computer Engineering Student" />
+            <meta name="twitter:title" content="boris jobs Portfolio - Computer Engineering Student" />
             <meta name="twitter:description"
-                content="Vivek Patel's (vivek9patel) Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS." />
-            <meta name="twitter:site" content="vivek9patel" />
-            <meta name="twitter:creator" content="vivek9patel" />
-            <meta name="twitter:image:src" content="images/logos/logo_1024.png" />
+                content="boris jobs's (borisjobs) Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS." />
+            <meta name="twitter:site" content="borisjobs" />
+            <meta name="twitter:creator" content="borisjobs" />
+            <meta name="twitter:image:src" content="images/logos/alexander-modified.png" />
+            {/* logo_1024.png */}
             /* Open Graph general (Facebook, Pinterest & Google+) */
-            <meta name="og:title" content="Vivek Patel Portfolio - Computer Engineering Student" />
+            <meta name="og:title" content="boris jobs Portfolio - Computer Engineering Student" />
             <meta name="og:description"
-                content="Vivek Patel's (vivek9patel) Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS." />
-            <meta name="og:image" content="images/logos/logo_1200.png" />
-            <meta name="og:url" content="http://vivek9patel.github.io/" />
-            <meta name="og:site_name" content="Vivek Patel Personal Portfolio" />
+                content="boris jobs's (borisjobs) Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS." />
+            <meta name="og:image" content="images/logos/alexander-modified.png" />
+            {/* logo_1200.png */}
+            <meta name="og:url" content="http://borisjobs.github.io/" />
+            <meta name="og:site_name" content="boris jobs Personal Portfolio" />
             <meta name="og:locale" content="en_IN" />
             <meta name="og:type" content="website" />
 
-            <link rel="icon" href="images/logos/fevicon.svg" />
-            <link rel="apple-touch-icon" href="images/logos/logo.png" />
+            <link rel="icon" href="images/logos/alexander-modified.png" />
+            {/* fevicon.svg */}
+            <link rel="apple-touch-icon" href="images/logos/alexander-modified.png" />
+            {/* logo.png */}
             <link rel="preload" href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&display=swap" as="style" />
             <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&display=swap" rel="stylesheet"></link>
         </Head>
--- components/apps/spotify.js
+++ components/apps/spotify.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
 export default function Spotify() {
-    return (
-        // <iframe src="//player.bilibili.com/player.html?isOutside=true&aid=1353986541&bvid=BV13z421U7cs&cid=1525102356&p=1" frameBorder="0" title="Spotify" className="h-full w-full bg-ub-cool-grey"></iframe>
+    return (     
+        // <iframe src="https://www.eminem.com/song/lose-yourself-soundtrack-version/" frameBorder="0" title="The Most Influential Song for Me" className="h-full w-full bg-ub-cool-grey"></iframe>
         
         <iframe src="https://avrillavigne.com/" frameBorder="0" title="Avril Lavigne Official" className="h-full w-full bg-ub-cool-grey"></iframe>
     )
