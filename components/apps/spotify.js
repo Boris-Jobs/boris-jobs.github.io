c53fd19204fc2e80a93a26a4dfe9b5a7dc4749b3
boris sigma <1322553126@qq.com>
2024-09-08 14:48:47 +0800

    used the template of vivek

---
+++ components/apps/spotify.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Spotify() {
+    return (
+        // <iframe src="//player.bilibili.com/player.html?isOutside=true&aid=1353986541&bvid=BV13z421U7cs&cid=1525102356&p=1" frameBorder="0" title="Spotify" className="h-full w-full bg-ub-cool-grey"></iframe>
+        
+        <iframe src="https://avrillavigne.com/" frameBorder="0" title="Avril Lavigne Official" className="h-full w-full bg-ub-cool-grey"></iframe>
+    )
+}
+
+export const displaySpotify = () => {
+    <Spotify> </Spotify>
+}
