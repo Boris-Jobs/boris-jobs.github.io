615483ceb1369a6fe51040b618b3efa2cee29ad9
boris sigma <1322553126@qq.com>
2024-09-11 22:03:31 +0800

    Updates

---
+++ static/XrToEed6y-JrMjr5w8yA6/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/b637e9a5-96758481f1d32622.js","static/chunks/79-bd85fd422aa9fbbe.js","static/chunks/pages/index-f3364df23f44346c.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
